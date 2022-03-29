fd13614f56c4bc8dc3d8defafc30bb0b46f449d1
Júlio César de Andrade Macedo <98000665+juliomacedobsb@users.noreply.github.com>
2022-03-29 11:07:42 -0300

    Add files via upload
    aula 03 js

---
+++ aula3.js
@@ -0,0 +1,16 @@
+
+//function testevariables() {
+  //  let name = 'julio';
+    //console.log (name)
+}
+//function testglobal() {
+    //let name = 'benjamin'
+    //console.log(name);
+    
+}
+estevariables();
+testglobal();
+
+ 
+const imprimeNome = (nome) => console.log(nome);
+imprimeNome ('julim')
